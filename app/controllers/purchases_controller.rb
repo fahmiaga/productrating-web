3d2f737101880955131b039ba5dae3624d0932ab
fahmiaga <fahmi.aga@gmail.com>
2022-03-28 13:16:55 +0700

    update and delete purchase

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,7 +5,7 @@ class PurchasesController < ApplicationController
     case action_name.to_sym
     when :new, :create
       @purchase = @product.purchases.new
-    when :show
+    when :show, :edit, :destroy, :update
       @purchase = @product.purchases.find(params[:id])
     end
   end
@@ -18,16 +18,15 @@ class PurchasesController < ApplicationController
     # - For example, if `purchase.quantity` is 3, decrease `product.quantity` by 3
     # - Display an error if `product.quantity` is less than 0 (negative number)
 
-   if @product.quantity < 1 || @product.quantity < purchase_params[:quantity].to_i
-    flash[:error] = 'Something went wrong'
-    return render :new  
-   end
+    if check_quantity
+      return render :new  
+    end
    
-    @newQuantity = @product.quantity - purchase_params[:quantity].to_i
+    @new_quantity = @product.quantity - purchase_params[:quantity].to_i
 
     @purchase.assign_attributes(purchase_params)
     if @purchase.save
-       @product.update(:quantity => @newQuantity)
+       @product.update(:quantity => @new_quantity)
        redirect_to product_url(@product)
     else
       flash[:error] = @purchase.errors.full_messages.join(', ')
@@ -37,14 +36,30 @@ class PurchasesController < ApplicationController
 
   def edit
     # TODO: Show edit form
+   
   end
 
   def update
     # TODO: Update record (save to database)
+   
+    if check_quantity
+      return render :edit  
+    end
+    @new_quantity = @product.quantity - purchase_params[:quantity].to_i
+        puts @new_quantity
+        if @purchase.update(purchase_params)
+            @product.update(:quantity => @new_quantity)
+              redirect_to product_url(@product)
+        else
+            render :edit
+        end
   end
 
   def destroy
     # TODO: Delete record
+   @purchase.destroy
+   redirect_to product_url(@product)
+
   end
 
   def show
@@ -53,5 +68,11 @@ class PurchasesController < ApplicationController
   private
     def purchase_params
       params.require(:purchase).permit(:quantity, :delivery_address)
+    end
+
+    def check_quantity
+      if @product.quantity < 1 || @product.quantity < purchase_params[:quantity].to_i
+        flash[:error] = 'Something went wrong'
+       end
     end
 end  
